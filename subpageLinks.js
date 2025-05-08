5f7b7ea23359eb7c6707dc0c116db1e72b66f33d
Sam Lee <36284756+leimapapa@users.noreply.github.com>
2025-05-08 17:06:23 -0600

    Update subpageLinks.js

--- subpageLinks.js
+++ subpageLinks.js
@@ -85,5 +85,11 @@ const links = [{
 				description: "Edit an SVG's path points to a set number",
 				url: "/SVGPathPointConverter",
 				icon: `<svg xmlns="http://www.w3.org/2000/svg" class="tool-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>`
+			}    ,
+			{
+				title: "SVG to GIF",
+				description: "Convert an animated SVG into a GIF",
+				url: "/SVGtoGIF",
+				icon: `<svg xmlns="http://www.w3.org/2000/svg" class="tool-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"/><circle cx="8.5" cy="8.5" r="1.5"/><path d="M21 15l-5-5L5 21"/></svg>`
 			}      
 		];
